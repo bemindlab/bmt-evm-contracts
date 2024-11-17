5eb4d20529fbdad53bd62e7b3342733cf016ddc4
Pituk Kaewsuksai <info@bemind.tech>
2024-11-17 19:30:57 +0700

    chore: deployed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,31 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
-import dotenv from "dotenv";
-dotenv.config();
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
+import "@openzeppelin/hardhat-upgrades";
+import "@typechain/hardhat";
+import "dotenv/config";
+import "hardhat-deploy";
+import "hardhat-gas-reporter";
+import { removeConsoleLog } from "hardhat-preprocessor";
+import "hardhat-spdx-license-identifier";
+import "solidity-coverage";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     bsc: {
-      url: `https://bsc-dataseed.binance.org/`,
-      accounts: process.env.DEPLOYER_PRIVATE_KEY
-        ? [process.env.DEPLOYER_PRIVATE_KEY]
-        : [],
+      url: "https://bsc-dataseed.binance.org/",
+      saveDeployments: true,
+      tags: ["bsc"],
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
     },
     bscTest: {
-      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
-      accounts: process.env.DEPLOYER_PRIVATE_KEY
-        ? [process.env.DEPLOYER_PRIVATE_KEY]
-        : [],
+      url: "https://bsc-testnet.publicnode.com",
+      saveDeployments: true,
+      tags: ["bscTest"],
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
     },
     // hardhat: {
     //   chainId: 1337,
@@ -26,6 +35,26 @@ const config: HardhatUserConfig = {
     //   },
     // },
   },
+  spdxLicenseIdentifier: {
+    overwrite: true,
+    runOnCompile: true,
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+  preprocess: {
+    eachLine: removeConsoleLog(
+      (hre) =>
+        hre.network.name !== "hardhat" && hre.network.name !== "localhost"
+    ),
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
+  sourcify: {
+    enabled: true
+  }
 };
 
 export default config;
